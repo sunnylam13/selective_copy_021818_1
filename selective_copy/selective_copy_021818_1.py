5f15c9edbe2db34f8103a1866efbb6d176ab6e89
Sunny Lam <sunny.lam@gmail.com>
2018-02-18 16:54:21 -0500

    revised code

--- selective_copy/selective_copy_021818_1.py
+++ selective_copy/selective_copy_021818_1.py
@@ -87,18 +87,33 @@ def copy_file_sh(filename,src,dst):
 
 	shutil.copyfile(src, dst)
 
-def scanfolder(foldername_path):
+def scanfolder(foldername_path,dirPath):
 	# this function scans the parent folder and subfolders
 	# it then adds them to a list so that its files can be scanned individually
 
-	# argument `foldername_path` should actually be a string path to folder
+	# `foldername_path` should actually be a string path to folder
+	# `dirPath` - the directory path leading up to the folder's name (yet not including it), should be an absolute path I'd say
+	
+	# abs_cwd_file_path for foldername_path?
 
-	dirs = os.listdir(foldername_path) # list all files
+	dirs = os.listdir(foldername_path) # list all files of any kind (i.e. all file and folder names)
+	absPath = dirPath
+
+	folder_path_list = [] # a list to hold all finalized folder paths (not folder names)
+
+	for file in dirs:
+		new_path = os.path.join(absPath,file) # creates a path to the file/folder
+
+		if os.path.isdir(new_path): #if the file is a folder
+			folder_path_list.append(new_path) # add it to the list of folders with its full path name
+		else:
+			continue # otherwise skip and keep going
 
 def scanfile(foldername_path, filename, search_result_path):
 	# this function scans a file to see if it matches the file type/ending specified by the user
 	# os.walk(user_folder_input)?
 	# os.walk(folder_path_to_be_analyzed)
+	
 	# `foldername_path` - the path to the folder to be analyzed
 	# `search_result_path` - where you want your search results to go
 	# `filename` - the name of the file in question, not necessarily a path
