b0d162e0511d2856ff2862772f132cd34f3db289
Sunny Lam <sunny.lam@gmail.com>
2018-02-18 15:28:59 -0500

    revised code

--- selective_copy/selective_copy_021818_1.py
+++ selective_copy/selective_copy_021818_1.py
@@ -45,7 +45,16 @@ file_type_regex1 = re.compile(user_file_ext_input + "$")
 # os.walk() handles all the behind the scenes looping, including into the subfolders (without having to write a separate loop)
 
 abs_cwd_file_path = os.path.abspath('.') # set the destination file path to be the current working directory or cwd
-search_result_path = os.mkdir(os.path.join(abs_cwd_file_path,"search_results")) # os.path.join() will handle the `/` or `\` depending on OS
+
+if (os.path.join(abs_cwd_file_path,"search_results")): # if the search_results folder already exists
+	search_result_path = (os.path.join(abs_cwd_file_path,"search_results")) # os.path.join() will handle the `/` or `\` depending on OS
+	# simply set the path to the search_results folder...
+else: # otherwise create the folder and then set search_result_path
+	search_result_path = os.mkdir(os.path.join(abs_cwd_file_path,"search_results")) # os.path.join() will handle the `/` or `\` depending on OS
+	search_result_path = (os.path.join(abs_cwd_file_path,"search_results")) # os.path.join() will handle the `/` or `\` depending on OS
+	# simply set the path to the search_results folder...
+
+# search_result_path = os.mkdir(os.path.join(abs_cwd_file_path,"search_results")) # os.path.join() will handle the `/` or `\` depending on OS
 
 for foldername,subfolders,filenames in os.walk(user_folder_input):
 	for filename in filenames:
@@ -58,6 +67,7 @@ for foldername,subfolders,filenames in os.walk(user_folder_input):
 			dst_file_path_name = os.path.join(search_result_path,filename)
 			# copy the files from their current location into a new folder (see Scratch file for thoughts on where new folder should be)
 			# using `copyfile(src, dst)`
-			copyfile(src_file_path_name, dst_file_path_name)
+			# copyfile(src_file_path_name, dst_file_path_name)
+			copyfile(src_file_path_name, search_result_path)
 			
 
