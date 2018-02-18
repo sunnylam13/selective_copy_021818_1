184c18aab5ff9adeb597f585411d527a2338fdfb
Sunny Lam <sunny.lam@gmail.com>
2018-02-18 13:30:06 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Selective File Copy
+
+The program walks through a folder tree and seeks files with certain file extensions (i.e. pdf, jpg).  It then copies these files from where they are in to a new folder.
+
---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+config = {
+	'description': 'The program walks through a folder tree and seeks files with certain file extensions (i.e. pdf, jpg).  It then copies these files from where they are in to a new folder.',
+	'author': 'Sunny Lam',
+	'url': 'URL to get it at',
+	'download_url': 'Where to download it',
+	'author_email': 'sunny.lam@gmail.com',
+	'version': '0.1',
+	'install_requires': ['nose'],
+	'packages': ['os'],
+	'scripts': [],
+	'name': 'Selective File Copy'
+}
+
+setup(**config)
